14b48fafddc68b730d5903cbea3843cbca85af76
EHamaguchi <81289idpd@gmail.com>
2025-11-25 09:27:23 +0900

    おじゃま情報更新

--- game9/Ozyama_up.java
+++ game9/Ozyama_up.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Ozyama here.
+ * Write a description of class Ozyama_up here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Ozyama extends Actor
+public class Ozyama_up extends Actor
 {
     /**
-     * Act - do whatever the Ozyama wants to do. This method is called whenever
+     * Act - do whatever the Ozyama_up wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
